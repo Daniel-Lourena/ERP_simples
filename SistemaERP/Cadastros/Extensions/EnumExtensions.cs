2d6e2d02c721773ed53adebfc006776596ddc4f7
Daniel Lourena <dhlourena9@gmail.com>
2025-04-21 01:04:13 -0300

    Mudança para deixar generico a população do combobox
    Criei dessa maneira para utilizar sempre de forma mais prática, passando valores como listas e enums para popular automaticamente

---
+++ SistemaERP/Cadastros/Extensions/EnumExtensions.cs
@@ -0,0 +1,34 @@
+﻿using ModuloCadastro.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaERP.Cadastros.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static List<EnumItem> GetList<TEnum>() where TEnum : System.Enum
+        {
+            List<EnumItem> enumList = new();
+
+            foreach (object value in System.Enum.GetValues(typeof(TEnum)))
+            {
+                string name = value.ToString();
+                int intValue = (int)value;
+
+                FieldInfo fi = typeof(TEnum).GetField(name);
+                DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                string description = attributes.Length > 0 ? attributes[0].Description : name;
+
+                enumList.Add(new EnumItem { Name = name, Description = description, Value = intValue });
+            }
+
+            return enumList;
+        }
+    }
+}
---
+++ SistemaERP/Cadastros/Extensions/Helper/ComboBoxHelper.cs
@@ -0,0 +1,35 @@
+﻿using ModuloCadastro.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaERP.Cadastros.Extensions.Helper
+{
+    public static class ComboBoxHelper
+    {
+        public static void PreencherComboBoxList<T>(ComboBox comboBox,List<T> dataSource,string valueMember,string displayMember, bool selecionarPrimeiro = false) where T : class
+        {
+            comboBox.DataSource = null;
+            comboBox.DataSource = dataSource;
+            comboBox.DisplayMember = displayMember;
+            comboBox.ValueMember = valueMember;
+
+            if (selecionarPrimeiro && comboBox.Items.Count > 0)
+                comboBox.SelectedIndex = 0;
+        }
+        public static void PreencherComboBoxEnum<E>(ComboBox comboBox,bool selecionarPrimeiro = false) where E : Enum
+        {
+            List<EnumItem> dataSource = Extensions.EnumExtensions.GetList<E>();
+
+            comboBox.DataSource = null;
+            comboBox.DataSource = dataSource;
+            comboBox.ValueMember = nameof(EnumItem.Value);
+            comboBox.DisplayMember = nameof(EnumItem.Name);
+
+            if (selecionarPrimeiro && comboBox.Items.Count > 0)
+                comboBox.SelectedIndex = 0;
+        }
+    }
+}

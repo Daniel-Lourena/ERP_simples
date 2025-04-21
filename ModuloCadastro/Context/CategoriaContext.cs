9ba4a6d29a2d39f380df4cf21a21583450ee26dd
Daniel Lourena <dhlourena9@gmail.com>
2025-04-21 04:22:04 -0300

    Criação de categoria do produto

---
+++ ModuloCadastro/Context/CategoriaContext.cs
@@ -0,0 +1,17 @@
+﻿using ModuloCadastro.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCadastro.Context
+{
+    public class CategoriaContext
+    {
+        public List<CategoriaEntity> GetList()
+        {
+            return new ModuloCadastroContext().Categorias.ToList();
+        }
+    }
+}
---
+++ ModuloCadastro/Entity/CategoriaEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCadastro.Entity
+{
+    [Table("tb_categoria")]
+    public class CategoriaEntity : BaseEntity<CategoriaEntity>
+    {
+        [Key,Display(Name = "ID",Description = "")]
+        public int id { get; set; }
+        [Display(Name = "Descrição",Description = "")]
+        public string descricao { get; set; }
+    }
+}

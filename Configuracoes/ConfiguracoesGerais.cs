1d637ce05d72bf1c0ca4344f5e0cf5adfa90db9c
Daniel Lourena <dhlourena9@gmail.com>
2025-04-21 14:14:57 -0300

    Criação de Banco de dados através do Migrations

--- Configuracoes/ConfiguracoesGerais.cs
+++ Configuracoes/ConfiguracoesGerais.cs
@@ -2,6 +2,6 @@
 {
     public static class ConfiguracoesGerais
     {
-        public static string stringConexaoDB = "";
+        public static string stringConexaoDB = "Server=localhost;Port=3306;Database=erp_simples;Uid=root;Pwd=120302;";
     }
 }
---
+++ ModuloCadastro/Migrations/20250421165433_CriacaoBanco_erp_simples.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModuloCadastro.Migrations
+{
+    public partial class CriacaoBanco_erp_simples : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_autonumerador",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    idCliente = table.Column<int>(type: "int", nullable: false),
+                    idUsuario = table.Column<int>(type: "int", nullable: false),
+                    idProduto = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_autonumerador", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_categoria",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    descricao = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_categoria", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_cidade",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    cuf = table.Column<int>(type: "int", nullable: false),
+                    cmunicipio = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    dmunicipio = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_cidade", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_cliente",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    razaoSocial = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    fantasia = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    limiteCredito = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    end_nomeRua = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    end_bairro = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    end_numero = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    end_logradouro = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    end_uf = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    end_cidade = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    dataCadastro = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    dataAtualizacao = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    excluido = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    dataExclusao = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_cliente", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_estado",
+                columns: table => new
+                {
+                    cuf = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    uf = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_estado", x => x.cuf);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_produtos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    descricao = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    idUnidade = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    setorEstoque = table.Column<int>(type: "int", nullable: false),
+                    cest = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ncm = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    codigoEstoque_SKU = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    categoria = table.Column<int>(type: "int", nullable: false),
+                    estoqueMinimo = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    origem = table.Column<int>(type: "int", nullable: false),
+                    cst_csosn = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    inativo = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    usuarioCadastro = table.Column<int>(type: "int", nullable: false),
+                    dataCadastro = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    dataAtualizacao = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_produtos", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_setorestoque",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    descricao = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_setorestoque", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tb_usuario",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    cargo = table.Column<int>(type: "int", nullable: false),
+                    dataCadastro = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    dataAtualizacao = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    excluido = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    dataExclusao = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_usuario", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_autonumerador");
+
+            migrationBuilder.DropTable(
+                name: "tb_categoria");
+
+            migrationBuilder.DropTable(
+                name: "tb_cidade");
+
+            migrationBuilder.DropTable(
+                name: "tb_cliente");
+
+            migrationBuilder.DropTable(
+                name: "tb_estado");
+
+            migrationBuilder.DropTable(
+                name: "tb_produtos");
+
+            migrationBuilder.DropTable(
+                name: "tb_setorestoque");
+
+            migrationBuilder.DropTable(
+                name: "tb_usuario");
+        }
+    }
+}

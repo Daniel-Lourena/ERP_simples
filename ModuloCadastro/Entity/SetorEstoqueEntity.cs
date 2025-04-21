eb55f6a99eb680ac04d945227adabdd295ba0005
Daniel Lourena <dhlourena9@gmail.com>
2025-04-21 04:24:22 -0300

    criação do setor de estoque do produto

---
+++ ModuloCadastro/Entity/SetorEstoqueEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCadastro.Entity
+{
+    [Table("tb_setorestoque")]
+    public class SetorEstoqueEntity : BaseEntity<SetorEstoqueEntity>
+    {
+        [Key, Display(Name = "ID", Description = "")]
+        public int id { get; set; }
+        [Display(Name = "Descrição", Description = "")]
+        public string descricao { get; set; }
+    }
+}

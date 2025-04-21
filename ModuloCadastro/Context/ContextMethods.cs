3e0613fbbf4b4f0a87077218cbc6cac849282521
Daniel Lourena <dhlourena9@gmail.com>
2025-04-21 01:07:13 -0300

    Passagem de método UPDATE somente das propriedades declaradas para forma genérica utilizando o EF

---
+++ ModuloCadastro/Context/ContextMethods.cs
@@ -0,0 +1,25 @@
+﻿using ModuloCadastro.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCadastro.Context
+{
+    public static class ContextMethods
+    {
+        public static void UpdateParcial<T>(T entity, List<string> listaPropriedadesAtualizar) where T : class
+        {
+            var _context = new ModuloCadastroContext();
+
+            _context.Attach(entity);
+
+            listaPropriedadesAtualizar.
+                ForEach(propriedadeAtualiza =>
+                _context.Entry(entity).Property(propriedadeAtualiza).IsModified = true);
+
+            _context.SaveChanges();
+        }
+    }
+}

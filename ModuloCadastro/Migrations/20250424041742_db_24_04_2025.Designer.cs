c15afe831fd684cf1a591914065740dae03fdbd2
Daniel Lourena <dhlourena9@gmail.com>
2025-04-24 01:19:23 -0300

    Modificações aplicadas no database
    dotnet ef migrations add db_24_04_2025 --context ModuloCadastroContext --project "C:\ERP_simples\ModuloCadastro\ModuloCadastro.csproj"

--- ModuloCadastro/Migrations/20250424041742_db_24_04_2025.Designer.cs
+++ ModuloCadastro/Migrations/20250424041742_db_24_04_2025.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ModuloCadastro.Context;
 
@@ -10,9 +11,10 @@ using ModuloCadastro.Context;
 namespace ModuloCadastro.Migrations
 {
     [DbContext(typeof(ModuloCadastroContext))]
-    partial class ModuloCadastroContextModelSnapshot : ModelSnapshot
+    [Migration("20250424041742_db_24_04_2025")]
+    partial class db_24_04_2025
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

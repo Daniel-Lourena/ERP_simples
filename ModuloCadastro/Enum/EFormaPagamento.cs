53634b494ed4b03661e94ba7ed4213110db8876d
Daniel Lourena <dhlourena9@gmail.com>
2025-04-26 16:08:58 -0300

    Criação para Forma de Pagamento ( seleção dentro do pedido )

---
+++ ModuloCadastro/Enum/EFormaPagamento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCadastro.Enum
+{
+    public enum EFormaPagamento
+    {
+        [Description("DINHEIRO")]
+        DINHEIRO = 1,
+        [Description("CHEQUE")]
+        CHEQUE = 2,
+        [Description("BOLETO")]
+        BOLETO = 3,
+        [Description("TRANSFERÊNCIA")]
+        TRANSFERENCIA = 4,
+        [Description("CRÉDITO")]
+        CREDITO_LOJA = 5,
+        [Description("CARTÃO DÉDITO")]
+        CARTAO_DEBITO = 6,
+        [Description("CARTÃO CRÉDITO")]
+        CARTAO_CREDITO = 7
+    }
+}

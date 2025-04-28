13a4a7bb6c60f65ce072703e4e264bdfd5c891b2
Daniel Lourena <dhlourena9@gmail.com>
2025-04-27 23:49:36 -0300

    Criação de Entidade Estoque para controlar o estoque dos itens

---
+++ ModuloCadastro/Entity/EstoqueEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCadastro.Entity
+{
+    [Table("tb_estoque")]
+    public class EstoqueEntity : BaseEntity<EstoqueEntity>
+    {
+        [Display(Name = "idProduto", Description = ""), Column(TypeName = "int")]
+        public int idProduto { get; set; }
+        [Display(Name = "quantidade", Description = ""), Column(TypeName = "decimal(10,2)")]
+        public decimal quantidade { get; set; }
+        [Display(Name = "setorEstoque", Description = ""), Column(TypeName = "int")]
+        public int setorEstoque { get; set; }
+
+        public SetorEstoqueEntity DadosSetorEstoque { get; set; }
+        public ProdutoEntity DadosProduto{ get; set; }
+    }
+}

c170c15879c6c4b17901578f3415b2c6a5735197
Daniel Lourena <dhlourena9@gmail.com>
2025-05-04 02:59:51 -0300

    Retirada de atributo de display na classe de dominio do cliente

--- ModuloCadastro/Entity/ClienteEntity.cs
+++ ModuloCadastro/Entity/ClienteEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ModuloCadastro.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,41 +12,57 @@ namespace ModuloCadastro.Entity
     [Table("tb_clientes")]
     public class ClienteEntity : BaseEntity<ClienteEntity>
     {
-        [Key, Display(Name = "ID", Description = ""), Column(TypeName = "int"), DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key,Column(TypeName = "int"), DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int id { get; set; }
-        [Display(Name = "Razão Social", Description = ""), Column(TypeName = "varchar(255)")]
+        [Column(TypeName = "varchar(255)")]
         public string razaoSocial { get; set; }
-        [Display(Name = "Fantasia", Description = ""), Column(TypeName = "varchar(100)")]
+        [Column(TypeName = "varchar(100)")]
         public string fantasia { get; set; }
-        [Display(Name = "Limite Crédito", Description = ""), Column(TypeName = "decimal(10,2)")]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal limiteCredito { get; set; }
-        [Display(Name = "Rua", Description = ""), Column(TypeName = "varchar(255)")]
+        [Column(TypeName = "varchar(255)")]
         public string end_nomeRua { get; set; }
-        [Display(Name = "Bairro", Description = ""), Column(TypeName = "varchar(100)")]
+        [Column(TypeName = "varchar(100)")]
         public string end_bairro { get; set; }
-        [Display(Name = "N°", Description = ""), Column(TypeName = "varchar(20)")]
+        [Column(TypeName = "varchar(20)")]
         public string end_numero { get; set; }
-        [Display(Name = "Logradouro", Description = ""), Column(TypeName = "varchar(10)")]
+        [Column(TypeName = "varchar(10)")]
         public string end_logradouro { get; set; }
-        [Display(Name = "UF", Description = ""), Column(TypeName = "int")]
+        [Column(TypeName = "int")]
         public int end_uf { get; set; }
-        [Display(Name = "ID Cidade", Description = ""), Column(TypeName = "int")]
+        [Column(TypeName = "int")]
         public int end_cidade { get; set; }
-        [Display(Name = "Dta. Cadastro", Description = ""), Column(TypeName = "datetime")]
-        public DateTime dataCadastro { get; set; }
-        [Display(Name = "Dta. Atualização", Description = ""), Column(TypeName = "datetime")]
-        public DateTime dataAtualizacao { get; set; }
-        [Display(Name = "Excluído", Description = ""), Column(TypeName = "tinyint(1)")]
+        [Column(TypeName = "datetime")]
+        public DateTime? dataCadastro { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? dataAtualizacao { get; set; }
+        [Column(TypeName = "tinyint(1)")]
         public bool excluido { get; set; }
-        [Display(Name = "Dta. Exclusão", Description = ""), Column(TypeName = "datetime")]
+        [Column(TypeName = "datetime")]
         public DateTime? dataExclusao { get; set; }
 
         public CidadeEntity DadosCidade { get; set; }
 
-        [NotMapped]
-        public string enderecoCompleto
+        public ClienteViewModel ToViewModel()
         {
-            get { return $"{end_logradouro} {end_nomeRua} {end_numero},{end_cidade} - {end_uf}"; }
+            return new ClienteViewModel
+            {
+                id = this.id,
+                razaoSocial = this.razaoSocial,
+                fantasia = this.fantasia,
+                limiteCredito = this.limiteCredito,
+                end_nomeRua = this.end_nomeRua,
+                end_bairro = this.end_bairro,
+                end_numero = this.end_numero,
+                end_logradouro = this.end_logradouro,
+                end_uf = this.end_uf,
+                end_cidade = this.end_cidade,
+                dataCadastro = this.dataCadastro,
+                dataAtualizacao = this.dataAtualizacao,
+                excluido = this.excluido,
+                dataExclusao = this.dataExclusao,
+                DadosCidade = this.DadosCidade
+            };
         }
     }
 }

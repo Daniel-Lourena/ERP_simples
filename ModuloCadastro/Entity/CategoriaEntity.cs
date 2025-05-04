58f2427a9b476091f7abf8da71723ea671e89a96
Daniel Lourena <dhlourena9@gmail.com>
2025-05-04 00:38:39 -0300

    Alteração na model de categoria e usuario ( retirando atributo de descricao das propriedades )

--- ModuloCadastro/Entity/CategoriaEntity.cs
+++ ModuloCadastro/Entity/CategoriaEntity.cs
@@ -11,9 +11,9 @@ namespace ModuloCadastro.Entity
     [Table("tb_categoria")]
     public class CategoriaEntity : BaseEntity<CategoriaEntity>
     {
-        [Key,Display(Name = "ID",Description = ""), Column(TypeName = "int"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key,Column(TypeName = "int"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
-        [Display(Name = "Descrição",Description = ""), Column(TypeName = "varchar(100)")]
+        [Column(TypeName = "varchar(100)")]
         public string? descricao { get; set; }
     }
 }
--- ModuloCadastro/Entity/UsuarioEntity.cs
+++ ModuloCadastro/Entity/UsuarioEntity.cs
@@ -14,19 +14,19 @@ namespace ModuloCadastro.Entity
     [Table("tb_usuarios")]
     public class UsuarioEntity : BaseEntity<UsuarioEntity>
     {
-        [Display(Name = "ID",Description = ""), Column(TypeName = "int"), DatabaseGenerated(DatabaseGeneratedOption.None)] 
+        [Key,Column(TypeName = "int"), DatabaseGenerated(DatabaseGeneratedOption.None)] 
         public int id { get; set; }
-        [Display(Name = "Nome",Description = ""), Column(TypeName = "varchar(50)")] 
+        [Column(TypeName = "varchar(50)")] 
         public string nome { get; set; }
-        [Display(Name = "Cargo",Description = ""), Column(TypeName = "int")] 
+        [Column(TypeName = "int")] 
         public ECargo cargo { get; set; }
-        [Display(Name = "Dta. Cadastro", Description = ""), Column(TypeName = "datetime")] 
+        [Column(TypeName = "datetime")] 
         public DateTime? dataCadastro { get; set; }
-        [Display(Name = "Dta. Atualização", Description = ""), Column(TypeName = "datetime")] 
+        [Column(TypeName = "datetime")] 
         public DateTime? dataAtualizacao { get; set; }
-        [Display(Name = "Excluído", Description = ""), Column(TypeName = "tinyint(1)")] 
+        [Column(TypeName = "tinyint(1)")] 
         public bool excluido { get; set; }
-        [Display(Name = "Dta. Exclusão", Description = ""), Column(TypeName = "datetime")]
+        [Column(TypeName = "datetime")]
         public DateTime? dataExclusao { get; set; }
 
 

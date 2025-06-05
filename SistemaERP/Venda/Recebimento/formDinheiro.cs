caebed113daa2e753cd4f6611ab39a40defd3727
Daniel Lourena <dhlourena9@gmail.com>
2025-06-04 23:59:10 -0300

    Criação de form para adição de forma de recebimento em Dinheiro no pedido de venda

---
+++ SistemaERP/Venda/Recebimento/formDinheiro.cs
@@ -0,0 +1,46 @@
+﻿using ModuloCadastro.Entity;
+using ModuloCadastro.Service;
+using ModuloCadastro.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaERP.Venda.Recebimento
+{
+    public partial class formDinheiro : Form
+    {
+        private int _idPedido;
+        public formDinheiro(int idPedido)
+        {
+            InitializeComponent();
+            _idPedido = idPedido;
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (nudValor.Value <= 0)
+            {
+                MessageBox.Show($"Valor inválido.", "Sistema ERP", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            new RecebimentosVendaService().Insert(new RecebimentoVendaEntity()
+            {
+                Especie = ModuloCadastro.Enum.ERecebimentoEspecie.DINHEIRO,
+                NroParcela = 1,
+                TotalParcela = 1,
+                PedidoId = _idPedido,
+                Vencimento = DateTime.Now.Date,
+                Valor = nudValor.Value,
+                Descricao = txtObs.Text
+            });
+            this.Close();
+        }
+    }
+}

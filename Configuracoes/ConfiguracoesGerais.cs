3bdb7dfcbf2b63353025f5db748abbcf2367c5b0
Daniel Lourena <dhlourena9@gmail.com>
2026-04-13 23:47:57 -0300

    Atualizacao de string de acesso ao banco de dados modificada para obter os dados das variaveis de ambiente -> retirada a string hardcoded

--- Configuracoes/ConfiguracoesGerais.cs
+++ Configuracoes/ConfiguracoesGerais.cs
@@ -2,6 +2,8 @@
 {
     public static class ConfiguracoesGerais
     {
-        public static string stringConexaoDB = "Server=localhost;Port=3306;Database=erp_simples;Uid=root;Pwd=120302;Allow User Variables=true;";
+        public static string stringConexaoDB =>
+        Environment.GetEnvironmentVariable("DB_CONNECTION")
+        ?? throw new Exception("Variável de ambiente DB_CONNECTION não definida");
     }
 }
